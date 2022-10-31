21efdccf9cd69ce9e4f1a9a9c01ea8a5b25e2fb3
shogo <shogo.i-mf74@outlook.jp>
2022-10-31 20:06:49 +0900

    修正

--- trafficLight.playground/Contents.swift
+++ trafficLight.playground/Contents.swift
@@ -7,52 +7,58 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 class TrafficLight{
     var timer: Timer?
     var count: Int = 0
-    var lightColor: String = ""
     var limit: Int = 0
     var lightTimeList: [Int] = [7,4,2]
-    func start(lightColor: String){
-        self.lightColor = lightColor//self.lightColorとしないといけない
-//       while totalCount <= 5//ここで繰り返してしまうとtimerが重複してしまう。さらにinvalidateは1つを残して消滅
-            switch lightColor {
-            case "red":
-                print("信号が赤です")
-                limit = lightTimeList[0]
-            case "blue":
-                print("信号が青です")
-                limit = lightTimeList[1]
-            default:
-                print("信号が黄です")
-                limit = lightTimeList[2]
-            }
-            timer = Timer.scheduledTimer(
-                timeInterval: 1,
-                target: self,
-                selector: #selector(countup),
-                userInfo: nil,
-                repeats: true
-            )
-    }//func start閉じる
+    enum LightColor{
+        case red//(limit: Int)
+        case blue
+        case yellow
+    }
     
-    @objc func countup(){//引数は渡せれない。(シンタックシュガー勉強しよう)
-//間違い⇨ @objc func countup(lightColor: String){
+    var lightColorRed = LightColor.red
+    var lightColorBlue = LightColor.blue
+    var lightColorYellow = LightColor.yellow
+    
+    func start(lightColor: LightColor) {
+        switch lightColor {
+        case LightColor.red :
+            print("信号が赤です")
+            limit = lightTimeList[0]
+        case LightColor.blue:
+            print("信号が青です")
+            limit = lightTimeList[1]
+        default:
+            print("信号が黄です")
+            limit = lightTimeList[2]
+        }
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(countup),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc func countup(){
         count += 1
         print("残り\(limit-count)秒です")
         if limit <= count{
             print("信号が変わります")
-            let countEnd: Bool = limit == count
+            let isCountEnd: Bool = limit == count
             count = 0
-            switch lightColor{
-            case "red":
-                self.lightColor = "blue" //なんでself必要??
-            case "blue":
-                self.lightColor = "yellow"
+            switch lightColor{//⭐️.self追加
+            case LightColor.red:
+                lightColor = LightColor.blue
+            case LightColor.blue:
+                lightColor = LightColor.yellow
             default:
-                self.lightColor = "red"
+                lightColor = LightColor.red
             }
             timer?.invalidate()
-            if countEnd == true{
+            if isCountEnd {
                 let nextLight = NextLight()
-                nextLight.roop(lightColor: self.lightColor)
+                nextLight.roop(lightColor: lightColor)
             }
         }
     }
@@ -60,13 +66,13 @@ class TrafficLight{
 
 class NextLight{
     let trafficLight = TrafficLight()
-    func roop(lightColor: String){
-        print("\(lightColor)")//確認用
+    func roop(lightColor: TrafficLight.LightColor){
+        print("\(lightColor)")
         trafficLight.start(lightColor: lightColor)
     }
 }
 
-let lightColor: String = "red"
 let trafficLight = TrafficLight()
+var lightColor = TrafficLight.LightColor.red
 trafficLight.start(lightColor: lightColor)
 
